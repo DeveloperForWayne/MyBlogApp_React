72e47bad8f7154a5e81530a2538ac1c6d254bd08
Wayne.Xu <wayne.xu.work@gmail.com>
2018-12-06 16:33:08 -0500

    slice added

--- src/App.js
+++ src/App.js
@@ -7,34 +7,13 @@ import Footer from './components/footer'
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      blogData: [],
-    };
   }
 
-  componentDidMount() {
-    const url = `https://newsapi.org/v2/everything?q=bitcoin&from=2018-11-03&sortBy=publishedAt&apiKey=3943181eb2954a0bb8d20dc7f808032b`
-    fetch(url)
-      .then(response => response.json())
-      .then(data => data["articles"].map((article) => {
-        return {
-          title: article['title'],
-          content: article['content'],
-          author: article['author'],
-          url: article['url'],
-          urlToImage: article['urlToImage']
-        }
-      }))
-      .then((data) => {
-        this.setState({ blogData: data });
-      })
-  }
-
-  render() {
+   render() {
     return (
       <div className="App">
         <NavBar />
-        <BlogContents blogData={this.state.blogData}/>
+        <BlogContents />
         <Footer />
       </div>
     );
--- src/components/blogContents.js
+++ src/components/blogContents.js
@@ -3,21 +3,48 @@ import React, { Component } from 'react';
 class blogContents extends Component {
  
     constructor(props) {
-        super(props)
+        super(props);
+        this.state = {
+            blogData: [],
+        };
+    }
+
+    componentDidMount() {
+        const url = `https://newsapi.org/v2/everything?q=bitcoin&from=2018-11-13&sortBy=publishedAt&apiKey=3943181eb2954a0bb8d20dc7f808032b`
+        fetch(url)
+          .then(response => response.json())
+          .then(data => data["articles"].map((article) => {
+            return {
+              title: article['title'],
+              content: article['content'],
+              author: article['author'],
+              url: article['url'],
+              urlToImage: article['urlToImage']
+            }
+          }))
+          .then((data) => {
+            this.setState({ blogData: data });
+          })
+    }
+
+    handleClick() {
+        this.setState(state => ({
+         
+        }));
     }
 
     render() {
         return (
             <div className="row">
                 <div className="col col-xs-12">
-                    {this.props.blogData.map(blog=>
+                    {this.state.blogData.map(blog=>
                     <div className="blog-grids">
                         <div className="grid">
                             <div className="entry-body">
-                                <h3>{blog.title}</h3>
-                                <p>{blog.content}</p>
+                                <h3>{blog.title.slice(0, 30)  + "..."}</h3>
+                                <p>{blog.content.slice(0, 100)  + "..."}</p>
                                 <div className="read-more-date">
-                                    <a href={blog.url}>Read More..</a>
+                                    <a onClick={this.handleClick.bind(this)} href={blog.url}>Read More..</a>
                                 </div>
                             </div>
                         </div>
